a6fc4b38504e1ede329e3dac97475ff19c3300e5
bladeclara42 <afiif87@gmail.com>
2024-07-30 17:35:52 +0700

    add models

---
+++ app/Models/Book.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'author_id',
+        'publisher_id',
+        'condition_id',
+        'type',
+        'price',
+        'published_year',
+    ];
+
+    public function author()
+    {
+        return $this->belongsTo(Author::class);
+    }
+
+    public function publisher()
+    {
+        return $this->belongsTo(Publisher::class);
+    }
+
+    public function condition()
+    {
+        return $this->belongsTo(BookCondition::class);
+    }
+
+    public function orderItems()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+}
